a5fccdb4969a5986c1f863adeadfc3309d82080e
Sahil <sahilpjoshi270@gmail.com>
2025-10-17 12:00:21 +0530

    Processing AI response

--- aiService/src/main/java/com/fitness/aiService/service/ActivityMessageListner.java
+++ aiService/src/main/java/com/fitness/aiService/service/ActivityMessageListner.java
@@ -2,6 +2,8 @@ package com.fitness.aiService.service;
 
 
 import com.fitness.aiService.model.Activity;
+import com.fitness.aiService.model.Recommendation;
+import com.fitness.aiService.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,11 +15,14 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListner {
 
     private final ActivityAIService aiService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
 
         log.info("Received activity for processing {}",activity.getId());
-        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+//        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        Recommendation aiRecommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(aiRecommendation);
     }
 }
