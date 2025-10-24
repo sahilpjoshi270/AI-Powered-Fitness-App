60baf745b91e4f8a119c1bc479f100820b3bd6b4
Sahil <sahilpjoshi270@gmail.com>
2025-10-24 23:03:59 +0530

    24th Oct ReactJs

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -3,10 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router";
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { AuthContext } from 'react-oauth2-code-pkce';
 import { useDispatch } from 'react-redux';
 import { setCredentials } from './store/authSlice';
+
+const ActivitiesPage = () => {
+  <Box component="section" sx={{ p: 2,border: '2px dashed grey'}} >
+    <ActivityForm onActivitiesAdded = {() => window.location.reload()}/>
+    <ActivityList />
+  </Box>
+}
 
 function App() {
   const {token, tokenData, logIn, logOut, isAuthenticated } = useContext(AuthContext);
@@ -28,9 +35,17 @@ function App() {
               logIn();        
           }}>LOGIN</Button>
           ) : (
-            <div>
-              <pre>{JSON.stringify(tokenData,null, 2)}</pre>
-            </div>
+            // <div>
+            //   <pre>{JSON.stringify(tokenData,null, 2)}</pre>
+            // </div>
+            <Box component="section" sx={{ p: 2,border: '2px dashed grey'}} > 
+              <Routes>
+                <Route path='/activities' element={<ActivitiesPage/>}/>
+                <Route path='/activities/:id' element={<ActivityDetail/>}/>
+                <Route path='/' element={token ? <Navigate to="/activities" replace/> : <div>Welcome! Please Login</div>}/>
+                
+              </Routes>
+            </Box>
           )}
     </Router>
   )
---
+++ fitness-app-frontend/src/components/ActivityList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ActivityList = () => {
+    return 
+    <div>ActivityList</div>
+}
+
+export default ActivityList
